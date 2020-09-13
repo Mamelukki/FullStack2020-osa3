e13576839521fc7c5fe471ce440e275ecf778a78
Salmi Sara M <salmisar@cs.helsinki.fi>
2020-09-13 15:33:08 +0300

    Muutokset Herokua varten

---
+++ build/precache-manifest.8303ec2552772282879b0ac2fc5508c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9854e7ed9210d08f17ac5c6b94f3057",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e55f9b04324a23bc260",
+    "url": "/static/css/main.d68247be.chunk.css"
+  },
+  {
+    "revision": "5b495e56d259e1add482",
+    "url": "/static/js/2.3793ae4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3793ae4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e55f9b04324a23bc260",
+    "url": "/static/js/main.593dfe7d.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
